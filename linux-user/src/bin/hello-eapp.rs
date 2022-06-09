99b6a65c42fea9b619aad9daa91cd7cb3f76e909
ayf19 <ayf19@mails.tsinghua.edu.cn>
2022-06-09 19:36:42 +0800

    bug fix

--- linux-user/src/bin/hello-eapp.rs
+++ linux-user/src/bin/hello-eapp.rs
@@ -1,15 +1,22 @@
 #![no_std]
 #![no_main]
+#![feature(default_alloc_error_handler)]
 
-#[macro_use]
-extern crate linux_user;
-extern crate alloc;
+use buddy_system_allocator::LockedHeap;
 
-use alloc::string::String;
-use alloc::vec::Vec;
+#[global_allocator]
+static HEAP: LockedHeap = LockedHeap::empty();
+use keystone_rust_sdk::{ocall, eapp_ret};
 
 #[no_mangle]
-fn main(args: Vec<String>) -> i32  {
-    println!("Hello world from user!");
-    0
+fn main() -> ! {
+    let data = [0u8; 32];
+    let mut ret  = [0u8; 32];
+    ocall(8, data.as_ptr(), 32, ret.as_mut_ptr(), 32);
+    eapp_ret(0)
+}
+
+#[panic_handler]
+fn panic_handler(_: &core::panic::PanicInfo) -> ! {
+    loop {}
 }
